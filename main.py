e734b479b077ab27c1eb0151a46acb9dcebe934c
amirnak <amirnakgamer@gmail.com>
2025-06-22 02:08:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from config import settings
+
+bot = Bot(token=settings.BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start(message: Message) -> None:
+    await message.answer("Привет")
+
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
