ff852f04c9e89312109c5146cc1498edbf82c071
amirnak <amirnakgamer@gmail.com>
2025-07-06 00:41:42 +0300

    Fix: use pathlib for config

--- bot/config.py
+++ bot/config.py
@@ -1,10 +1,16 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+CURRENT_DIR = Path(__file__).resolve().parent
 
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
 
-    model_config = SettingsConfigDict(env_file="./.env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=CURRENT_DIR / ".env", env_file_encoding="utf-8"
+    )
 
 
 settings = Settings()  # type: ignore
